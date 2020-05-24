f180e8a7bf4869f66fad5f6d818d64ee2a5ef65f
sh2268411762 <2268411762@qq.com>
2020-05-25 00:20:32 +0800

    C++第二章

---
+++ C++课本/二.2整数相加.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <stdlib.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int num1(0);
+//	int num2(0);
+//	int sum(0);
+//
+//	cout << "请输入第一个数：";
+//	cin >> num1;
+//
+//	cout << "请输入第二个数：";
+//	cin >> num2;
+//
+//	sum = num1 + num2;
+//
+//	//cout << "num1 + num2 = " << sum << endl;
+//	cout << num1 << "+" << num2 << "=" << sum << endl;
+//	return EXIT_SUCCESS;
+//}
---
+++ C++课本/二.3if语句.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+//#include <stdlib.h>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int num1(0);
+//	int num2(0);
+//
+//	cout << "输入两个整数进行比较：";
+//	cin >> num1 >> num2;
+//
+//	if (num1 == num2)
+//	{
+//		cout << num1 << "等于" << num2 << endl;
+//	}
+//	if (num1 != num2)
+//	{
+//		cout << num1 << "不等于" << num2 << endl;
+//	}
+//	if (num1 < num2)
+//	{
+//		cout << num1 << "小于" << num2 << endl;
+//	}
+//	if (num1 > num2)
+//	{
+//		cout << num1 << "大于" << num2 << endl;
+//	}
+//	if (num1 <= num2)
+//	{
+//		cout << num1 << "小于等于" << num2 << endl;
+//	}
+//	if (num1 >= num2)
+//	{
+//		cout << num1 << "大于等于" << num2 << endl;
+//	}
+//	return EXIT_SUCCESS;
+//}
